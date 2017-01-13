d3d8cea53eb76386442cdb42e3137f7c761fc2a1
LAPTOP-KOEN\Koen <koen.vankralingen@gmail.com>
2017-01-13 16:59:22 +0100

    favicon toegevoegd

--- Admin/overzicht.php
+++ Admin/overzicht.php
@@ -30,6 +30,8 @@
 		<meta http-equiv="X-UA-Compatible" content="IE=edge">
 		<meta name="viewport" content="width=device-width, initial-scale=1">
 		<meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+		<link rel="shortcut icon" href="favicon.ico" type="image/x-icon">
+		<link rel="icon" href="favicon.ico" type="image/x-icon">
 		<!-- The above 3 meta tags *must* come first in the head; any other head content must come *after* these tags -->
 
 		<!-- Bootstrap core CSS -->
