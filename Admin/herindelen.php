4b472ceed2599421a816fa70d79a7dafe0245b48
PAUL-PC\paul <244384@cursist.rockopnh.nl>
2017-01-14 21:13:22 +0100

    Het herindelen van de ouderavond, verder gegaan met de back-end. wat nu werkt: er wordt gecontroleerd of de leerling kan op die tijd, er wordt gecontroleerd of de zelfde leerlingnummer op volgend zijn. wat nog moet; de uren opnieuw indelen in de database, uitgebreider testen.

--- Admin/herindelen.php
+++ Admin/herindelen.php
@@ -46,8 +46,7 @@ if(isset($_GET["ID"])){
     <script src="../bootstrap/js/vendor/jquery.min.js"></script>
     <script src="../bootstrap/js/bootstrap.min.js"></script>
 
-    <!-- de scripte die gebruikt worden voor de sorteer funcite -->
-    <script type="text/javascript" src="redips-drag-min.js"></script>
+
 
     <link href="style.css" rel="stylesheet">
 </head>
@@ -61,25 +60,44 @@ if(isset($_GET["ID"])){
     </div>
 </nav>
 
-<div class="container-fluid">
+<div class="container-fluid margin_bottom">
     <div class="page-header text-center ">
         <h1>de ouderavond opnieuw indelen</h1>
         <p class="font_size">sleep de tijden in de goede volgorde.</p>
+        <p> dit is de ouderavond voor:
+            <b>
+            <?php
+                //haalt de docent gegevens op
+                $sqli_docent_naam = "SELECT Voornaam, Achternaam FROM docenten WHERE Docent_ID = '".$_SESSION["Docent_ID"]."'";
+                $sqli_docent_naam_uitkomst = mysqli_query($connect, $sqli_docent_naam);
+                $docent_naam = mysqli_fetch_array($sqli_docent_naam_uitkomst);
+                echo $docent_naam["Voornaam"] . " " . $docent_naam["Achternaam"];
+                echo " op " . $_SESSION["Datum"];
+            ?>
+            </b>
+        </p>
     </div>
     <div class="col-md-8 col-md-offset-2">
 
         <?php
+
+        //zet alles binnen een formulier.
+        echo "<form action='' method='post' class='no_padding  text-center'>";
         //geeft alle opties qua leerlingen die moeten worden ingedeelt.
         echo "<div id='redips-drag'>";
-        echo "<table class='table table-condensed margin_bottom' id='table1'>";
+        echo "<table class='table table-condensed margin_bottom ' style='font-size: 15px' id='table1'>";
+            echo "<colgroup>";
+                echo "<col width='240'/>";
+                echo "<col width='240'/>";
+            echo "</colgroup>";
             echo "<tr>";
-                echo "<th colspan='2' class='redips-drag t1'>$Docent_Naam</th>";
+                echo "<th colspan='2' id='message' class='redips-drag t1 status_ding'>Status</th>";
             echo "</tr>";
             echo "<tr>";
-                echo "<td>";
+                echo "<td class='redips-mark desc'>";
                     echo "<p>Begin tijd</p>";
                 echo "</td>";
-                echo "<td>";
+                echo "<td  class='redips-mark desc'>";
                     echo "<p>Leerling</p>";
                 echo "</td>";
             echo "</tr>";
@@ -88,85 +106,206 @@ if(isset($_GET["ID"])){
         $sqli_gegevens_uitkomst = mysqli_query($connect, $sqli_gegevens);
         while($row = mysqli_fetch_array($sqli_gegevens_uitkomst)){
             echo "<tr>";
-                echo "<td>";
+                echo "<td class='redips-mark desc'>";
                     echo "<p>".$row['Begin_Tijd']."</p>";
                 echo "</td>";
                 echo "<td>";
-                    echo "<div id='link1' class='redips-drag t1'>".$row["Leerling_ID"]."</div>";
+                    //geeft het tijdslot als hidden input (wordt gebruikt voor de her indeling
+                    echo "<input type='hidden' name='Tijd_Slot[]' value='".$row["Tijd_Slot"]."'>";
+
+                    //kan slecht telezen zijn. voor exstra info ga naar: Paul (244384)
+                    echo "<div id='link1' class='redips-drag t1'>";
+                        //controleerd of het leerling id 0 is, als het 0 wordt er Leeg weergegeven.
+                        if($row["Leerling_ID"] == 0){
+                            echo "beschikbaar.";
+                        }
+                        else{
+                            echo $row["Leerling_ID"];
+                        }
+                        echo "<input type='hidden' name='Leerling[]' value='".$row["Leerling_ID"]."'>";
+
+                    echo "</div>";
                 echo "</td>";
             echo "</tr>";
         }
         echo "</table>";
+        echo "<input type='submit' value='submit' name='submit' class='col-md-offset-2 col-md-8 btn btn-primary'>";
         echo "</div>";
+        echo "</form>";
+        echo "<br>";
 
+        if(isset($_POST["submit"])){
+            //var_dump($_POST["Leerling"]);
+            //echo "<br>";echo "<br>";
+            //var_dump($_POST["Tijd_Slot"]);
+            //echo "<br>";echo "<br>";
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-        echo "<div class='col-md-2' ondrop='drop(event)' ondragover='allowDrop(event)' ondragstart='drag(event)' style='min-height: 100px; min-width: inherit'>";
-            //maakt de naam goed.(en haalt hem op) (SLECHT TE LEZEN SORRY)
-            $sqli_leerling_naam = "SELECT leerlingen.Leerling_ID, leerlingen.Voornaam, leerlingen.Voorvoegsel, leerlingen.Achternaam FROM tijden_binnen_avond JOIN leerlingen ON tijden_binnen_avond.Leerling_ID = leerlingen.Leerling_ID WHERE tijden_binnen_avond.Docent_ID='".$_SESSION["Docent_ID"]."' AND tijden_binnen_avond.Datum = '".$_SESSION["Datum"]."' AND afgerond='0' ";
-            $sqli_leerling_naam_uitkomst = mysqli_query($connect, $sqli_leerling_naam);
-            while($row1 = mysqli_fetch_array($sqli_leerling_naam_uitkomst)) {
-                if (!empty($row1["Voorvoegsel"])) {$naam = $row1['Voornaam'] . "&nbsp" . $row1["Voorvoegsel"] . "&nbsp" . $row1['Achternaam'];}
-                elseif (!empty($row1["Voornaam"])) {$naam = $row1['Voornaam'] . "&nbsp" . $row1['Achternaam'];}
-                else {$naam = "Vrij";}
-                //geeft de naam als een drag en drop
-                echo "<p id='drag' draggable='true' ondragstart='drag(event)'>".$naam."</p>";
+            $koekjes = count($_POST["Leerling"]);
+            //var_dump($koekjes);
+            for($X = 0; $X < $koekjes; $X++){
+                //var_dump($_POST["Leerling"][$X]);
+                if($_POST["Leerling"][$X] != 0){
+                    //controleerd of er meerder van de zelfde waarde zijn binnen de array.
+                    $check_Voor_opvoling = HEEFT_DUPE($X);
+                    if($check_Voor_opvoling == 2){
+                        //er zijn 2x de zelrde waarde gevonden.
+                        //echo "er zijn er 2";
+                        //controleerd of de volgende ook 2 is.
+                        if($_POST["Leerling"][$X] == $_POST["Leerling"][$X + 1]){
+                            //controleerd of de leeling niet op de geselecteerde tijd als is ingeschreven.
+                            //om dit te controleren moeten we eerst de gegevens hebben.
+                            $sqli_inschrijvings_gegevens = "SELECT Tijd_Slot, Begin_Tijd, Eind_Tijd, Datum, Leerling_ID FROM tijden_binnen_avond WHERE Tijd_Slot = '".$_POST["Tijd_Slot"][$X]."'";
+                            $row = mysqli_fetch_array(mysqli_query($connect, $sqli_inschrijvings_gegevens));
+                            //contoleerd of de leerling kan op die tijd.
+                            $sqli_tijd_check = "SELECT Tijd_Slot FROM tijden_binnen_avond WHERE Leerling_ID='".$_POST["Leerling"][$X]."' AND Begin_Tijd='".$row["Begin_Tijd"]."' AND Datum='".$row["Datum"]."' AND Tijd_Slot!='".$row["Tijd_Slot"]."'";
+                            $sqli_tijd_check_uitkomst = mysqli_query($connect, $sqli_tijd_check);
+                            if(mysqli_num_rows($sqli_tijd_check_uitkomst) == 0){
+                                //controleerd of de leeling niet op de geselecteerde tijd als is ingeschreven.
+                                //om dit te controleren moeten we eerst de gegevens hebben.
+                                $sqli_inschrijvings_gegevens = "SELECT Tijd_Slot, Begin_Tijd, Eind_Tijd, Datum, Leerling_ID FROM tijden_binnen_avond WHERE Tijd_Slot = '".$_POST["Tijd_Slot"][$X + 1]."'";
+                                $row = mysqli_fetch_array(mysqli_query($connect, $sqli_inschrijvings_gegevens));
+                                //contoleerd of de leerling kan op die tijd.
+                                $sqli_tijd_check = "SELECT Tijd_Slot FROM tijden_binnen_avond WHERE Leerling_ID='".$_POST["Leerling"][$X + 1]."' AND Begin_Tijd='".$row["Begin_Tijd"]."' AND Datum='".$row["Datum"]."' AND Tijd_Slot!='".$row["Tijd_Slot"]."'";
+                                $sqli_tijd_check_uitkomst = mysqli_query($connect, $sqli_tijd_check);
+                                if(mysqli_num_rows($sqli_tijd_check_uitkomst) == 0){
+                                    $Alle_Checks_Goed = true;
+                                }
+                                else{
+                                    $Alle_Checks_Goed = false;
+                                    $Leerling_Kan_Niet_Op_Datum = true;
+                                    break;
+                                }
+                            }
+                            else{
+                                $Alle_Checks_Goed = false;
+                                $Leerling_Kan_Niet_Op_Datum = true;
+                                break;
+                            }
+                            //de volgende is ook 2, die die hoeft niet gecontroleerd te worden. dus ver hoog de X waarde met 1.
+                            $X ++;
+                        }
+                        else{
+                            $Alle_Checks_Goed = false;
+                            $Niet_Op_Volgend = true;
+                            break;
+                        }
+                    }
+                    elseif($check_Voor_opvoling == 3){
+                        //er zijn 3x de zelrde waarde gevonden.
+                        //echo "er zijn er 3";
+                        //controleerd of de volgende 2 ook de zelfde leerling zijn.
+                        if($_POST["Leerling"][$X] == $_POST["Leerling"][$X + 1] && $_POST["Leerling"][$X] == $_POST["Leerling"][$X + 2]){
+                            //controleerd of de leeling niet op de geselecteerde tijd als is ingeschreven.
+                            //om dit te controleren moeten we eerst de gegevens hebben.
+                            $sqli_inschrijvings_gegevens = "SELECT Tijd_Slot, Begin_Tijd, Eind_Tijd, Datum, Leerling_ID FROM tijden_binnen_avond WHERE Tijd_Slot = '".$_POST["Tijd_Slot"][$X]."'";
+                            $row = mysqli_fetch_array(mysqli_query($connect, $sqli_inschrijvings_gegevens));
+                            //contoleerd of de leerling kan op die tijd.
+                            $sqli_tijd_check = "SELECT Tijd_Slot FROM tijden_binnen_avond WHERE Leerling_ID='".$_POST["Leerling"][$X]."' AND Begin_Tijd='".$row["Begin_Tijd"]."' AND Datum='".$row["Datum"]."' AND Tijd_Slot!='".$row["Tijd_Slot"]."'";
+                            $sqli_tijd_check_uitkomst = mysqli_query($connect, $sqli_tijd_check);
+                            //var_dump($sqli_tijd_check); echo "<br>";
+                            if(mysqli_num_rows($sqli_tijd_check_uitkomst) == 0){
+                                //controleerd of de leeling niet op de geselecteerde tijd als is ingeschreven.
+                                //om dit te controleren moeten we eerst de gegevens hebben.
+                                $sqli_inschrijvings_gegevens = "SELECT Tijd_Slot, Begin_Tijd, Eind_Tijd, Datum, Leerling_ID FROM tijden_binnen_avond WHERE Tijd_Slot = '".$_POST["Tijd_Slot"][$X + 1]."'";
+                                $row = mysqli_fetch_array(mysqli_query($connect, $sqli_inschrijvings_gegevens));
+                                //contoleerd of de leerling kan op die tijd.
+                                $sqli_tijd_check = "SELECT Tijd_Slot FROM tijden_binnen_avond WHERE Leerling_ID='".$_POST["Leerling"][$X + 1]."' AND Begin_Tijd='".$row["Begin_Tijd"]."' AND Datum='".$row["Datum"]."' AND Tijd_Slot!='".$row["Tijd_Slot"]."'";
+                                $sqli_tijd_check_uitkomst = mysqli_query($connect, $sqli_tijd_check);
+                                //var_dump($sqli_tijd_check); echo "<br>";
+                                if(mysqli_num_rows($sqli_tijd_check_uitkomst) == 0){
+                                    //controleerd of de leeling niet op de geselecteerde tijd als is ingeschreven.
+                                    //om dit te controleren moeten we eerst de gegevens hebben.
+                                    $sqli_inschrijvings_gegevens = "SELECT Tijd_Slot, Begin_Tijd, Eind_Tijd, Datum, Leerling_ID FROM tijden_binnen_avond WHERE Tijd_Slot = '".$_POST["Tijd_Slot"][$X + 2]."'";
+                                    $row = mysqli_fetch_array(mysqli_query($connect, $sqli_inschrijvings_gegevens));
+                                    //contoleerd of de leerling kan op die tijd.
+                                    $sqli_tijd_check = "SELECT Tijd_Slot FROM tijden_binnen_avond WHERE Leerling_ID='".$_POST["Leerling"][$X + 2]."' AND Begin_Tijd='".$row["Begin_Tijd"]."' AND Datum='".$row["Datum"]."' AND Tijd_Slot!='".$row["Tijd_Slot"]."'";
+                                    $sqli_tijd_check_uitkomst = mysqli_query($connect, $sqli_tijd_check);
+                                    //var_dump($sqli_tijd_check); echo "<br>";
+                                    if(mysqli_num_rows($sqli_tijd_check_uitkomst) == 0){
+                                        $Alle_Checks_Goed = true;
+                                    }
+                                    else{
+                                        $Alle_Checks_Goed = false;
+                                        $Leerling_Kan_Niet_Op_Datum = true;
+                                        break;
+                                    }
+                                }
+                                else{
+                                    $Alle_Checks_Goed = false;
+                                    $Leerling_Kan_Niet_Op_Datum = true;
+                                    break;
+                                }
+                            }
+                            else{
+                                $Alle_Checks_Goed = false;
+                                $Leerling_Kan_Niet_Op_Datum = true;
+                                break;
+                            }
+                            //de volgende is ook 2, die die hoeft niet gecontroleerd te worden. dus ver hoog de X waarde met 2.
+                            $X = $X + 2;
+                        }
+                        else{
+                            $Alle_Checks_Goed = false;
+                            $Niet_Op_Volgend = true;
+                            break;
+                        }
+                    }
+                    else{
+                        //er is maar 1 keer de waarde gevonden.
+                        //controleerd of de leeling niet op de geselecteerde tijd als is ingeschreven.
+                        //om dit te controleren moeten we eerst de gegevens hebben.
+                        $sqli_inschrijvings_gegevens = "SELECT Tijd_Slot, Begin_Tijd, Eind_Tijd, Datum, Leerling_ID FROM tijden_binnen_avond WHERE Tijd_Slot = '".$_POST["Tijd_Slot"][$X]."'";
+                        $row = mysqli_fetch_array(mysqli_query($connect, $sqli_inschrijvings_gegevens));
+                        //contoleerd of de leerling kan op die tijd.
+                        $sqli_tijd_check = "SELECT Tijd_Slot FROM tijden_binnen_avond WHERE Leerling_ID='".$_POST["Leerling"][$X]."' AND Begin_Tijd='".$row["Begin_Tijd"]."' AND Datum='".$row["Datum"]."' AND Tijd_Slot!='".$row["Tijd_Slot"]."'";
+                        $sqli_tijd_check_uitkomst = mysqli_query($connect, $sqli_tijd_check);
+                        if(mysqli_num_rows($sqli_tijd_check_uitkomst) == 0){
+                            $Alle_Checks_Goed = true;
+                        }
+                        else{
+                            $Alle_Checks_Goed = false;
+                            $Leerling_Kan_Niet_Op_Datum = true;
+                            break;
+                        }
+                    }
+                }
+                //om te voorkomen dat er een execution time out komt
+                set_time_limit(0);
             }
 
-        echo "</div>";
-
-        //geeft alle sloten weer.
-        echo "<div class='col-md-offset-2 col-md-2'>";
-            //maakt de naam goed.(en haalt hem op) (SLECHT TE LEZEN SORRY)
-            $sqli_sloten = "SELECT Tijd_Slot, Begin_Tijd, Eind_Tijd FROM tijden_binnen_avond WHERE Afgerond=0 AND Docent_ID='".$_SESSION["Docent_ID"]."' AND Datum='".$_SESSION["Datum"]."'";
-            $sqli_sloten_uitkomst = mysqli_query($connect, $sqli_sloten);
-            while($row = mysqli_fetch_array($sqli_sloten_uitkomst)){
-                echo  "<div class='' ondrop='drop(event)' ondragover='allowDrop(event)' ondragstart='drag(event)' style='height: 100px;width: calc(inherit/2); text-align: center; border: 1px solid black;'></div>";
+            if(isset($Niet_Op_Volgend)){
+                if($Niet_Op_Volgend == true){
+                    echo "de leerling: " . $_POST["Leerling"][$X] . " is niet opvolgend ingedeeld.<br>";
+                    echo "alle nummer die het zelfde zijn moeten elkaar opvolgen.";
+                }
             }
-        echo "</div>";
 
+            if(isset($Leerling_Kan_Niet_Op_Datum)){
+                if($Leerling_Kan_Niet_Op_Datum == true){
+                    echo "de leerling: " . $_POST["Leerling"][$X] . " kan niet op de tijd die u heeft gekozen.<br>";
+                    echo "(de tijd die u had gekozen: " . $row["Begin_Tijd"] . ")";
+                }
+            }
 
-
-
-
-        //var_dump($row);
-
-        echo "<h3>De planning van : ". strtoupper(substr($row["Voornaam"], 0, 1)) . ". " . $row["Achternaam"] ."</h3>";
-
-
-            //haalt alle inschrijvings gegevens op (gerelateerd aan de docent die gekozen is bij het verlengen.()
-            $sqli_gegevens = "SELECT Tijd_Slot, Datum, Begin_Tijd, Eind_Tijd FROM tijden_binnen_avond WHERE Afgerond=0 AND Docent_ID='".$_SESSION["Docent_ID"]."' AND Datum='".$_SESSION["Datum"]."'";//ALLE SLOTEN
-            $sqli_gegevens_uitkomst = mysqli_query($connect, $sqli_gegevens);
-            // maatk de hele tabel een list.
-            echo "<ul id='List_Sort' class='List_Sort' >";
-            while($row = mysqli_fetch_array($sqli_gegevens_uitkomst)){
-                        echo $row["Begin_Tijd"];
+            if(isset($Alle_Checks_Goed)){
+                if($Alle_Checks_Goed == true){
+                    echo "test.";
+                }
             }
 
 
-                echo "<li> " . $naam . "</li >";
+        }
 
 
-
-
-
-        echo "</ul>"; //sluit de lijst af.
-
-
-
-
+        function HEEFT_DUPE($X){
+            $count = 0;
+            foreach($_POST["Leerling"] as $value){
+                if($value == $_POST["Leerling"][$X]){
+                    $count ++;
+                }
+            }
+            return $count;
+        }
         ?>
     </div>
 </div>
@@ -177,6 +316,8 @@ if(isset($_GET["ID"])){
     &copy; Koen van Kralingen, Paul Backs, Mike de Decker en Jesse van Bree.
 </div>
 
+<!-- de scripte die gebruikt worden voor de sorteer funcite -->
+<script type="text/javascript" src="redips-drag-min.js"></script>
 <script type="text/javascript">
     // after page is loaded, initialize DIV elements inside table
     window.onload = function () {
