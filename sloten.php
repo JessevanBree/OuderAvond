f9ad3a9f8f8b494fda9bb2e70a73cb87c41ef7b6
PAUL-PC\paul <244384@cursist.rockopnh.nl>
2017-01-24 14:06:38 +0100

    BUG FIX: docenten met maar 1 dag worden nu goed weergegeven. (eerst werden er dagen weergegeven waar ze helemaal nooit zouden zijn.

--- sloten.php
+++ sloten.php
@@ -8,14 +8,14 @@ setlocale(LC_ALL, 'nl_NL');
 
         <?php
             //vraagt alle mogelijke sloten op van de docent uit de database
-            $sqli_Docent = "SELECT DISTINCT Datum FROM tijden_binnen_avond WHERE Afgerond=0";
+            $sqli_Docent = "SELECT DISTINCT Datum FROM tijden_binnen_avond WHERE Afgerond=0 AND Docent_ID='$ID'";
             $sqli_Docent_Uitkomst = mysqli_query($connect, $sqli_Docent);
             //zet het aantal dagen dat de ouderavond is in een variable
             $Aantal_Dagen = mysqli_num_rows($sqli_Docent_Uitkomst);
             while($row = mysqli_fetch_array($sqli_Docent_Uitkomst)){
                 //berekent de breete van de div aan de hand van hoeveel dagen er zijn
                 $Width = round(12/$Aantal_Dagen, 0, PHP_ROUND_HALF_DOWN);
-                echo "<div class='col-md-'".$Width."''>";
+                echo "<div class='col-md-12 no_padding'>";
                     echo "<table class='table'>";
                         echo "<tr>";
                             echo "<th>Dag</th>";
